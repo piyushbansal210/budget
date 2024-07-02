1deb1ae92d9091fcdceab4794fc165b58dfb2968
piyush.bansal <piyush.bansal@khumbusystems.com>
2024-07-02 19:18:57 +0530

    changes

--- ios/SpendSyncTests/SpendSyncTests.m
+++ ios/SpendSyncTests/SpendSyncTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface budgetTests : XCTestCase
+@interface SpendSyncTests : XCTestCase
 
 @end
 
-@implementation budgetTests
+@implementation SpendSyncTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
